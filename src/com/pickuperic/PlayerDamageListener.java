fb036ef3fce0a5d86076fdedc8fc0f29608ab9b7
Eric <pickup@uwindsor.ca>
2018-04-22 23:08:46 -0400

    Disabled PvP in spawn area

--- src/com/pickuperic/PlayerDamageListener.java
+++ src/com/pickuperic/PlayerDamageListener.java
@@ -1,5 +1,6 @@
 package com.pickuperic;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,11 +13,7 @@ public class PlayerDamageListener implements Listener {
 	@EventHandler
 	public void onDamage(EntityDamageByEntityEvent event) {
 		
-		if (Main.pvpEnabled == false) {
-			event.setCancelled(true);
-			event.getDamager().sendMessage(ChatColor.RED + "PvP is currently disabled!");
-		}
-		
+		//Check for team-hitting
 		if (event.getDamager() instanceof Player && event.getEntity() instanceof Player) {	//If the attacker and victim are players
 			
 			Player attacker = (Player) event.getDamager();
@@ -25,8 +22,23 @@ public class PlayerDamageListener implements Listener {
 			if (Teams.getPlayerTeam(attacker).equals(Teams.getPlayerTeam(victim))) {
 				attacker.sendMessage(ChatColor.RED + "You cannot attack your own team members!");
 				event.setCancelled(true);
+				return;
 			}
-			
+
+		}
+		
+		//Check if PVP is enabled
+		if (event.getDamager() instanceof Player && Main.pvpEnabled == false) {
+			event.setCancelled(true);
+			event.getDamager().sendMessage(ChatColor.RED + "PvP is currently disabled!");
+			return;
+		}
+		
+		//Check if PvP is inside spawn
+		if (event.getEntity() instanceof Player && event.getEntity().getLocation().distance(event.getEntity().getWorld().getSpawnLocation()) < 20) {
+			event.getDamager().sendMessage(ChatColor.RED + "PvP is disabled in spawn!");
+			event.setCancelled(true);
+			return;
 		}
 		
 	}

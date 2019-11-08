a0e8c2edfa021579b4cf0097483ec98327885ac9
Eric Pickup <pickup@uwindsor.ca>
2019-11-07 23:52:49 -0500

    Prevent block under flag from being broken

--- src/ca/uwindsor/css/ctf/FlagIndirectBreakListener.java
+++ src/ca/uwindsor/css/ctf/FlagIndirectBreakListener.java
@@ -2,10 +2,12 @@ package ca.uwindsor.css.ctf;
 
 import java.util.Iterator;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Tag;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
 import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.block.BlockPhysicsEvent;
@@ -18,7 +20,7 @@ public class FlagIndirectBreakListener implements Listener {
 	public void onPhysics(BlockPhysicsEvent event) {
 		
 		Block eventBlock = event.getBlock();
-		if (Tag.BANNERS.isTagged(eventBlock.getType())) {	//If banner is destroyed
+		if (Tag.BANNERS.isTagged(eventBlock.getType()) || Bukkit.getWorld(eventBlock.getWorld().getName()).getBlockAt(eventBlock.getLocation().add(0, 1, 0)).getType().toString().contains("BANNER")) {	//If banner is destroyed
 			event.setCancelled(true);
 		}
 	}
@@ -28,7 +30,7 @@ public class FlagIndirectBreakListener implements Listener {
 		
 		Block eventBlock = event.getBlock();
 		
-		if (Tag.BANNERS.isTagged(eventBlock.getType())) {	//If banner is destroyed
+		if (Tag.BANNERS.isTagged(eventBlock.getType()) || Bukkit.getWorld(eventBlock.getWorld().getName()).getBlockAt(eventBlock.getLocation().add(0, 1, 0)).getType().toString().contains("BANNER")) {	//If banner is destroyed
 			event.setCancelled(true);
 		}
 	}
@@ -37,7 +39,7 @@ public class FlagIndirectBreakListener implements Listener {
 	public void blockBurn(BlockBurnEvent event) {
 		Block eventBlock = event.getBlock();
 		
-		if (Tag.BANNERS.isTagged(eventBlock.getType())) {	//If banner is destroyed
+		if (Tag.BANNERS.isTagged(eventBlock.getType()) || Bukkit.getWorld(eventBlock.getWorld().getName()).getBlockAt(eventBlock.getLocation().add(0, 1, 0)).getType().toString().contains("BANNER")) {	//If banner is destroyed
 			event.setCancelled(true);
 		}
 	}
@@ -47,21 +49,25 @@ public class FlagIndirectBreakListener implements Listener {
 		Iterator<Block> it = event.blockList().iterator();
 		while (it.hasNext()) {
 		    Block b= it.next();
-		    if (Tag.BANNERS.isTagged(b.getType())) {
+		    if (Tag.BANNERS.isTagged(b.getType()) || Bukkit.getWorld(b.getWorld().getName()).getBlockAt(b.getLocation().add(0, 1, 0)).getType().toString().contains("BANNER")) {
 		    	it.remove();
 		    }
 		}
 	}
 	
 	@EventHandler
-	public void entityExplode(EntityExplodeEvent event) {
-		Iterator<Block> it = event.blockList().iterator();
+	public void entityExplode(EntityExplodeEvent e) {
+		Iterator<Block> it = e.blockList().iterator();
 		while (it.hasNext()) {
 		    Block b= it.next();
-		    if (Tag.BANNERS.isTagged(b.getType())) {
+		    if (Tag.BANNERS.isTagged(b.getType()) || Bukkit.getWorld(b.getWorld().getName()).getBlockAt(b.getLocation().add(0, 1, 0)).getType().toString().contains("BANNER")) {
 		    	it.remove();
 		    }
 		}
 	}
 	
+	@EventHandler
+	public void blockUnderFlagBreaks(BlockBreakEvent e) {
+		if (Bukkit.getWorld(e.getBlock().getWorld().getName()).getBlockAt(e.getBlock().getLocation().add(0, 1, 0)).getType().toString().contains("BANNER")) e.setCancelled(true);  
+	}
 }
